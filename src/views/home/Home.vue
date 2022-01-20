46f2c248a38d262a576554ed74a79b9265381469
BuildTools <unconfigured@null.spigotmc.org>
2022-01-20 17:06:11 -0500

    feat(views): basic home view

---
+++ src/views/home/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1 :class="$style.welcome">Welcome</h1>
+    <h2>Shayne Preston</h2>
+    <h3>I build Web Apps</h3>
+
+    <p>
+      I'm a Software Engineer who strives to build clean and inuitive apps for the web. I am currently working at FactSet Research Systems.
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Home',
+})
+</script>
+
+<style lang="scss" module>
+@use '~styles/colors';
+
+.welcome {
+  color: colors.$orange-red-crystal;
+}
+</style>
