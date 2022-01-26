9a682e1341d37bdd85789fc713817b6dfecb45cd
Shayne Preston <shpreston@optonline.net>
2022-01-26 12:59:04 -0500

    feat(about): non-coding content

--- src/views/about/About.vue
+++ src/views/about/About.vue
@@ -1,19 +1,22 @@
 <template>
   <div :class="$style.aboutView">
     <div :class="$style.descriptionContainer">
-      <h1 :class="$style.name">About Me</h1>
+      <h1 :class="$style.aboutHeader">About Me</h1>
 
       <div :class="$style.historyContainer">
         <!-- eslint-disable-next-line vue/max-len -->
-        <p>Hey! My name is Shayne, I&#39;m a Software Engineer, and I am obsessed with the web. My passion for web development began in High School where I took an Introduction to Programming class. I learned about HTML and CSS and loved the creativity and possibilities that a webpage could bring.</p>
+        <p>Hey&#33; My name is Shayne&#44; I&#39;m a Software Engineer&#44; and I am obsessed with the web&#46; My passion for web development began in high school where I took an Introduction to Programming class&#46; Through that class I learned about HTML and CSS. I loved the creativity and possibilities that a webpage could bring&#46;</p>
 
         <!-- eslint-disable-next-line vue/max-len -->
-        <p>With that taste of programming I decided to pursue a degree in Computer and Systems Engineering at Rensselaer Polytechnic Institute. I used my time there to explore different aspects of computer engineering from computer architecture in classes, to mobile app development in <a :class="$style.link" href="https://rcos.io/" target="_blank">RCOS</a>.</p>
+        <p>With that taste of programming I decided to pursue a degree in Computer and Systems Engineering at Rensselaer Polytechnic Institute&#46; I used my time there to explore different aspects of computer engineering from computer architecture in classes&#44; to mobile app development in <a :class="$style.link" href="https://rcos.io/" target="_blank">RCOS</a>&#46;</p>
       
         <!-- eslint-disable-next-line vue/max-len -->
-        <p>These days I&#39;m working on web based applications both on the frontend and on the backend at <a :class="$style.link" href="https://www.factset.com/" target="_blank">FactSet Research Systems</a>. These apps cater to thousands of users daily and are a cornerstone to FactSet&#39;s infrastructure.</p>
+        <p>These days I&#39;m working on web based applications both on the frontend and on the backend at <a :class="$style.link" href="https://www.factset.com/" target="_blank">FactSet Research Systems</a>&#46; These apps cater to thousands of users daily and are a cornerstone to FactSet&#39;s infrastructure&#46;</p>
 
-        <span>Technologies I&#39;m currently working with:</span>
+        <!-- eslint-disable-next-line vue/max-len -->
+        <p>Outside of engineering I love being outdoors&#33; In the summer you can catch me outside camping or at the beach&#44; and in the winter you&#39;ll find me skiing with friends and family&#46;</p>
+
+        <span>Technologies I&#39;m currently working with&#58;</span>
         <div :class="$style.technologies">
           <ul>
             <li>Javascript (ES6+)</li>
@@ -146,11 +149,11 @@ export default defineComponent({
   }
 }
 
-.name {
+.aboutHeader {
   color: colors.$orange-red-crystal;
   font-size: 5rem;
   font-weight: 900;
-  margin-top: 0;
+  margin: 0;
 }
 
 .historyContainer {
